34489912a1c6150dc4954dc3c6c62db659dd3c75
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2024-01-13 11:44:56 +0200

    Ported my function library to C++

---
+++ Public/GeneralFunctionLibrary.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GeneralFunctionLibrary.generated.h"
+
+/// A bunch of general Blueprint utils
+UCLASS()
+class UTILITY_API UGeneralFunctionLibrary : public UBlueprintFunctionLibrary {
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	static void Billboard(USceneComponent* target);
+	
+	UFUNCTION(BlueprintCallable)
+	static void Joeboard(USceneComponent* target);
+	
+	UFUNCTION(BlueprintCallable)
+	static void GoofyAhhBoard(USceneComponent* target);
+};
